7c44d529c145446e9a94e2d68824c0e13b690251
Marco Emrich <m.emrich@webmasters.de>
2018-09-10 09:01:18 +0200

    complete string_calc kata
     * negatives throw error
     * allow for custom separator

--- string_calc.spec.ts
+++ string_calc.spec.ts
@@ -1,10 +1,52 @@
-import {map, pipe, split, sum} from "ramda";
+import { map, pipe, split, sum, reduce, add, filter, gt, __, lt, ifElse, has, contains, any, match, replace } from "ramda";
 
 
-const calc : (string) => Number = pipe(
-  split(','), 
-  map(Number), 
-  sum);
+const mySum = reduce(add, 0);
+
+const commaSeparatedNumbersFromString = sep => pipe(split(sep), map(Number));
+
+const negative = lt(__, 0);
+
+const anyNegative: (l: number[]) => boolean = 
+  any(negative)
+
+const sumOfUnder1000s: (l: number[]) => number = pipe(filter(lt(__, 1000)), mySum);
+
+const throwNegativeNumberError = () => { throw Error("Contains a negative number!"); };
+
+const extractSeparator = str => match(/^\/\/(.)\//, str)[1] || ','
+
+const removePrefix = str => replace(/^\/\/.\//, '', str)
+
+const calc: (str: string) => Number =
+  str => pipe(
+    removePrefix,
+    commaSeparatedNumbersFromString(extractSeparator(str)), 
+    ifElse( 
+      anyNegative, 
+      throwNegativeNumberError, 
+      sumOfUnder1000s
+  ))(str);
+
+
+describe('removePrefix', () => {
+  it('should return a string without Prefix', () => {
+    expect(removePrefix("//x/1x2x3")).toEqual("1x2x3");
+  });
+  it('should return the whole string if there is no Prefix', () => {
+    expect(removePrefix("1,2,3")).toEqual("1,2,3");
+  });
+
+});
+
+describe('extractSeparator', () => {
+  it('should find a separator', () => {
+    expect(extractSeparator("//x/1x2x3")).toEqual("x");
+  });
+  it('should have a default of ,', () => {
+    expect(extractSeparator("1,2,3")).toEqual(",");
+  });
+});
 
 
 describe('calc', () => {
@@ -19,6 +61,18 @@ describe('calc', () => {
   it('should return the sum of multiple numbers', () => {
     expect(calc("1,2,3")).toEqual(6);
   })
+
+  it('should only add numbers < 1000', () => {
+    expect(calc("1,2,3,1000")).toEqual(6);
+  });
+
+  it('should throw an exception for negative numbers', () => {
+    expect(() => calc("1,-1,2")).toThrowError();
+  });
+
+  it('should allow a custom separator', () => {
+    expect(calc("//x/1x2x3")).toEqual(6);
+  });
 
 });
 
